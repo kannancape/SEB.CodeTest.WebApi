7a1bb3c2077c7ecc2b0a47bcc421d2048e77c599
karas <kannan.ramaswamy@hm.com>
2022-05-16 22:08:25 +0200

    SQL - File has added correctly.

---
+++ CodeTest.Sql.Backup/UserInfoDetails.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[UserInfoDetails] (
+    [Id]                       UNIQUEIDENTIFIER   NOT NULL,
+    [SocialSecurityNumber]     NVARCHAR (MAX)     NOT NULL,
+    [Emailaddress]             NVARCHAR (MAX)     NULL,
+    [PhoneNumber]              NVARCHAR (MAX)     NULL, 
+    [CreatedBy]                NVARCHAR (MAX)     NULL,  
+    [UpdatedBy]                NVARCHAR (MAX)     NULL,    
+    [CreatedDateTime]          DATETIMEOFFSET (7) NULL,
+    [UpdatedDateTime]          DATETIMEOFFSET (7) NULL,
+    [Deleted]                  BIT                NOT NULL
+     
+);
+
+
+ 
+
